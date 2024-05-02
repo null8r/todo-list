a2d7a89b38fc4c9db0751f5f7b6926245e457bf1
null <91502871+null8R@users.noreply.github.com>
2024-05-02 22:58:56 +0900

    VueJSでToDoリスト作成

---
+++ src/main.js
@@ -0,0 +1,28 @@
+new Vue({
+  el: "#todo",
+  data: {
+    newTask: "",
+    todos: [
+      { task: "example1", isCompleted: false },
+      { task: "example2", isCompleted: false }
+    ]
+  },
+  methods: {
+    addTodo: function(){
+      if(this.newTask == ""){
+        alert("newTask is empty");
+
+        return;
+      }
+      this.todos.push(
+        { task: this.newTask, isCompleted: false }
+      );
+      this.newTask = "";
+    },
+    deleteTodo: function(todo){
+      if(window.confirm("Delete?") == true){
+        this.todos.splice(todo, 1);
+      }
+    }
+  }
+});
